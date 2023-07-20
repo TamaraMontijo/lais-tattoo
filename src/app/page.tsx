b6c6ecb32dc6893a66d034d1cd5bc247562dd856
Tamara Montijo <tamaramontijo@wizsolucoes.com.br>
2023-07-20 17:12:12 -0300

    add button component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
      <p>Lais Tattoo</p>
-    <Button></Button>
+    <Button text="saiba mais" href='https://www.instagram.com/laiss.tattoo/' target='_blank'></Button>
      
     </main>
   )
