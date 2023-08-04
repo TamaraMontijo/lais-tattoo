aa4f9350b38873d5b11853b83dd2a5ffa9a570d9
Tamara Montijo <tamaramontijo@wizsolucoes.com.br>
2023-08-04 11:43:27 -0300

    section 2

--- src/sections/Section2.tsx
+++ src/sections/Section2.tsx
@@ -11,23 +11,23 @@ const crimson = Crimson({
 function Section2() {
   return (
     <div className="bg-palette-100 w-screen h-[120vh] relative ">
-      <div className="bg-palette-50 flex absolute top-1/2 right-1/2 translate-x-1/2 translate-y-[-50%] w-[55vw] h-[58vh]">
+      <div className="bg-palette-50 flex absolute top-1/2 right-1/2 translate-x-1/2 translate-y-[-50%] w-[40vw] h-[85vh]">
         <div className="content flex">
-          <div className="text">
-            <p className="uppercase font-barlow">favorite blogspot</p>
-            <p className="lowercase font-montserrat">simplify your wardrobe</p>
-            <p className="font-montserrat">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean 
+          <div className="text w-[366px] translate-x-[-25%] translate-y-[20%] h-auto">
+            <p className="uppercase font-barlow font-semibold leading-loose tracking-wider mb-6">favorite blogspot</p>
+            <p className="lowercase font-montserrat font-thin text-6xl w-[23vw] mb-6">simplify your wardrobe</p>
+            <p className="font-montserrat font-light leading-[1.8] tracking-widest text-base text-justify">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean 
               commodo ligula eget dolor. Lorem ipsum dolor sit amet, consectetuer 
               adipiscing elit. Aenean commodo ligula eget dolor. Lorem ipsum dolor 
               sit amet, consectetuer adipiscing elit. 
             </p>
           </div>
-          <div className="image">
+          <div className="image translate-x-[25%] translate-y-[20%] w-[400px]">
             <Image
               src="/processo-criativo.jpg"
               className="object-fit"
               alt="Imagem tatuagem de flor"
-              width={500}
+              width={800}
               height={500}
             />
           </div>
