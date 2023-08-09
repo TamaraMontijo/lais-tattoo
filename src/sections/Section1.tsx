a6468d0f071d4ffc2c9cd5f09d7d5ce4e34d99f1
Tamara Montijo <tamaramontijo@wizsolucoes.com.br>
2023-08-09 10:34:35 -0300

    section1

--- src/sections/Section1.tsx
+++ src/sections/Section1.tsx
@@ -13,32 +13,32 @@ function Section1() {
   return (
     <div className="bg-palette-50 w-screen h-[150vh] relative ">
       <div className="bg-palette-100 flex flex-col absolute top-1/2 right-1/2 translate-x-1/2 translate-y-[-50%] w-[30vw] h-[85vh]">
-        <div className="content flex mb-10">
-          <div className="image  w-[400px]">
+        <div className="content flex mb-10 items-end">
+          <div className="image  w-[400px] mr-20">
             <Image
               src="/studio2.jpg"
-              className="object-fit translate-x-[-30%] translate-y-[-50%]"
+              className="object-fit w-96"
               alt="foto do estúdio"
               width={800}
               height={500}
             />
           </div>
-          <div className="text w-[220px] translate-y-[20%] translate-x-[50%] h-auto">
-            <p className="lowercase font-montserrat font-thin text-6xl w-[23vw] mb-6 tracking-[0.03em]">photos</p>
+          <div className="text">
+            <p className="lowercase font-montserrat font-thin text-6xl mb-6 tracking-[0.03em]">photos</p>
             <p className="font-montserrat font-light leading-[1.8] tracking-widest text-base text-justify mb-6">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.  
             </p>
             
             <Button
-              text="get details"
+              text="SOBRE mim"
               href="https://www.instagram.com/laiss.tattoo/"
               target="_blank"
             ></Button>
       
           </div>
         </div>
-        <div className="content flex">
-          <div className="text w-[220px] translate-y-[20%] translate-x-[-50%] h-auto">
-            <p className="lowercase font-montserrat font-thin text-6xl w-[23vw] mb-6 tracking-[0.03em]">details</p>
+        <div className="content flex mb-10 items-end">
+          <div className="text mr-20">
+            <p className="lowercase font-montserrat font-thin text-6xl mb-6 tracking-[0.03em]">details</p>
             <p className="font-montserrat font-light leading-[1.8] tracking-widest text-base text-justify mb-6">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.  
             </p>
             
@@ -52,7 +52,7 @@ function Section1() {
           <div className="image  w-[400px]">
               <Image
                 src="/processo-criativo.jpg"
-                className="object-fit translate-x-[30%] translate-y-[20%]"
+                className="object-fit"
                 alt="Imagem tatuagem de flor"
                 width={800}
                 height={500}
