dd673256cb9c27fabc58c73822090f06fd402b67
Tamara Montijo <tamara.montijo@gmail.com>
2023-08-24 11:04:35 -0300

    button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,17 +8,18 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between w-screen">
       <Header></Header>
+      <div className="absolute bottom-0 fixed z-50">
+        <Button
+          text="Fale comigo"
+          href="https://api.whatsapp.com/send?phone=556199880877&text=Ol%C3%A1!%20Gostaria%20de%20solicitar%20um%20or%C3%A7amento."
+          target="_blank"
+        ></Button>
+      </div>
       <AboutMe></AboutMe>
       <Section1></Section1>
       <Section2></Section2>
 
-      {/* <div className="button absolute bottom-0">
-        <Button
-          text="saiba mais"
-          href="https://www.instagram.com/laiss.tattoo/"
-          target="_blank"
-        ></Button>
-      </div> */}
+      
     </main>
   );
 }
