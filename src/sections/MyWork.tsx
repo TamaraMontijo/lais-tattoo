2c336edc0824b3dcf74b687905966b627fede7f8
Tamara Montijo <tamara.montijo@gmail.com>
2023-09-04 17:23:59 -0300

    responsive md my work section

--- src/sections/MyWork.tsx
+++ src/sections/MyWork.tsx
@@ -14,26 +14,26 @@ function MyWork() {
   return (
     <div>
       {/* LARGE SCREEN */}
-    <div className="bg-palette-50 lg:w-screen h-[120vh] relative md:max-lg:flex md:max-lg:items-center md:max-lg:justify-center">
+    <div className="bg-palette-50 w-screen h-[120vh] relative md:max-lg:flex md:max-lg:items-center md:max-lg:justify-center">
       <div className="bg-[#817d7d] flex md:max-lg:w-[70%] md:max-lg:h-[60%] lg:absolute lg:top-1/2 lg:right-1/2 lg:translate-x-1/2 lg:translate-y-[-50%] lg:w-[30vw] lg:h-[85vh]">
         <div className="flex md:max-lg:items-center">
           <div className="image md:w-[400px]">
             <Image
               src="/processo-criativo.jpg"
-              className="object-fit md:translate-x-[-30%] lg:translate-y-[20%]"
+              className="object-fit lg:translate-x-[-30%] md:translate-x-[-20%] lg:translate-y-[30%]"
               alt="Imagem tatuagem de flor"
               width={800}
               height={500}
             />
           </div>
-          <div className="text w-[330px] lg:translate-y-[10%] h-auto">
+          <div className="text w-[330px] md:max-lg:translate-x-[-20%] lg:translate-y-[10%] h-auto md:max-lg:ml-[5%]">
             <p className="uppercase font-barlow font-semibold leading-loose tracking-wider mb-4">
               processo criativo
             </p>
-            <p className="lowercase font-montserrat font-thin text-6xl w-[23vw] mb-6 tracking-[0.03em]">
+            <p className="lowercase font-montserrat font-thin text-6xl md:max-lg:text-5xl w-[23vw] mb-6 tracking-[0.03em]">
               o meu trabalho
             </p>
-            <p className="font-montserrat font-light leading-[1.8] tracking-widest text-base text-justify  md:max-lg:w-[390px]">
+            <p className="font-montserrat font-light leading-[1.8] tracking-widest text-base text-justify  md:max-lg:w-[335px]">
             Ao longo dos últimos cinco anos, tenho tido a honra de transformar histórias em arte por meio da tatuagem. Meus traços finos e delicados ganham vida na pele dos meus clientes, representando momentos preciosos. Localizado em Águas Claras, meu estúdio é um espaço dedicado à qualidade, onde priorizamos os melhores materiais e técnicas para alcançar resultados de excelência em cada obra que criamos juntos.
             </p>
           </div>
