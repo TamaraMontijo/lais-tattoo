422cb3783fe696520719cbe2ea1b0be13261e90b
Tamara Montijo <tamara.montijo@gmail.com>
2023-09-08 11:54:10 -0300

    studio section for medium devices

--- src/sections/Studio.tsx
+++ src/sections/Studio.tsx
@@ -13,7 +13,7 @@ const crimson = Crimson({
 function Studio() {
   return (
     <div>
-      <div className="bg-[#817d7d] w-screen pt-8 pb-[16rem] lg:py-24 relative">
+      <div className="bg-[#817d7d] w-screen pt-8 pb-[16rem] md:max-lg:pb-[30rem] lg:py-24 relative">
         {/* left column */}
         <div className="w-3/4 lg:w-1/3 left-4 lg:left-40 top-1/4 relative">
           <h1 className="text-palette-300 tracking-widest font-crimson font-bold whitespace-nowrap text-6xl w-[23vw] mb-10 tracking-[0.03em]">
@@ -28,16 +28,16 @@ function Studio() {
           </p>
           {/* circle and line decoration */}
           <span className="w-3 h-3 bg-[#333333] rounded-full block absolute top-[4.75rem] left-[3.7rem]"></span>
-          <span className="w-0.5 h-[26rem] lg:h-[54rem] bg-[#333333] rounded-full block absolute top-[4.75rem] left-[4rem]"></span>
+          <span className="w-0.5 h-[26rem] md:h-80 lg:h-[54rem] bg-[#333333] rounded-full block absolute top-[4.75rem] left-[4rem]"></span>
           {/* end of circle and line decoration */}
           
           {/* Map */}
-          <div className="max-md:hidden mt-32 bg-palette-300 flex items-center justify-center p-16">
+          <div className="max-lg:hidden mt-32 bg-palette-300 flex items-center justify-center p-16">
             <Map></Map>
           </div>
         </div>
         {/* image */}
-        <div className="absolute flex flex-col max-lg:left-[5rem] top-[45%] lg:top-1/4 lg:left-1/2 lg:translate-y-[-50%] w-[60vw] lg:w-[30vw] h-[85vh]">
+        <div className="absolute flex flex-col max-lg:left-[5rem] top-[45%] md:max-lg:top-[25%] lg:top-1/4 lg:left-1/2 lg:translate-y-[-50%] w-[60vw] lg:w-[30vw] h-[85vh]">
           <Image
             src="/studio2.jpg"
             className="object-fit translate-y-[20%]"
@@ -45,12 +45,12 @@ function Studio() {
             width={800}
             height={500}
           />
-          <p className="mt-28 lg:mt-[400px] text-white font-montserrat text-base tracking-widest">Edifício Executive Office Tower</p>
+          <p className="mt-28 md:max-lg:mt-40 lg:mt-[400px] text-white font-montserrat text-base tracking-widest">Edifício Executive Office Tower</p>
           <p className="text-white tracking-widest font-light">SHN Q. 2 bloco F Sala 1111</p>
           <p className="text-white tracking-widest font-light"> Asa Norte, Brasília - DF, 70702-906</p>
         </div>
       </div>
-      <div className="bg-palette-300 w-screen flex p-8 md:hidden">
+      <div className="bg-palette-300 w-screen flex p-8 lg:hidden md:flex md:justify-center">
         <Map></Map>
       </div>
     </div>
