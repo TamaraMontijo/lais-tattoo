febb7d88ebfd4a114dbc0f24005c5ac23f776dd8
Tamara Montijo <tamara.montijo@gmail.com>
2023-09-14 10:45:57 -0300

    first section sm

--- src/sections/AboutMe.tsx
+++ src/sections/AboutMe.tsx
@@ -11,9 +11,9 @@ const crimson = Crimson({
 
 function AboutMe() {
   return (
-    <div className="lg:flex lg:flex-row h-screen w-full">
+    <div className="lg:flex lg:flex-row md:h-screen w-full max-md:relative max-md:top-32 max-md:h-[150vh]">
       <div className="lg:h-full bg-gray-900 md:w-1/2 relative">
-        <div className="image absolute inset-0 max-lg:top-[180px] max-lg:left-0 max-md:w-[260px] md:w-full max-md:h-[355px] md:h-[50vh] lg:h-full">
+        <div className="image absolute inset-0 max-md:top-[115px] max-lg:top-[180px] max-lg:left-0 max-md:w-[260px] md:w-full max-md:h-[355px] md:h-[50vh] lg:h-full">
           <Image
             src="/sobre.png"
             className="object-fit"
@@ -25,17 +25,17 @@ function AboutMe() {
       </div>
       <div className="h-full lg:w-1/2 bg-[#817d7d]">
         <div className="relative max-lg:left-[30%]">
-          <h1 className="font-crimson font-semibold text-5xl md:text-4xl text-white absolute lg:-left-1/4 max-md:top-56 top-48 max-md:w-[70%]">
+          <h1 className="font-crimson font-semibold text-[3rem] max-md:leading-9 md:text-4xl lg:text-5xl max-md:text-center text-white absolute lg:-left-1/4 max-md:top-56 top-48 max-md:w-[70%]">
             Cada traço que desenho conta <br />
             uma história única sobre você.
           </h1>
 
           <div className="w-full absolute top-80 flex flex-col justify-center ml-28">
             <div className="text-[#333333] w-1/2 justify-center items-center ">
-              <div className="border-solid border-l border-[#333333] pt-12 pl-8 relative">
-                <span className="w-3 h-3 bg-[#333333] rounded-full block absolute -top-1 -left-1.5"></span>
+              <div className="border-solid max-md:border-r md:border-l border-[#333333] max-md:pt-20 md:pt-12 pr-8 md:pl-8 relative max-md:right-[90px] max-md:mt-28">
+                <span className="w-3 h-3 bg-[#333333] rounded-full block absolute -top-1 -right-1.5 md:-left-1.5"></span>
 
-                <div className="flex items-center justify-center mb-14">
+                <div className="flex items-center justify-center md:mb-14">
                   <Image
                     src="../../logo.svg"
                     className="text-center"
@@ -46,7 +46,7 @@ function AboutMe() {
                 </div>
 
                 <div className="flex justify-center">
-                  <h3 className="font-montserrat text-base tracking-widest font-light mt-4 pb-14 text-white text-justify w-4/5">
+                  <h3 className="font-montserrat text-base tracking-widest font-light mt-4 pb-14 text-white text-justify w-auto md:w-4/5">
                     Sou movida pela paixão de tatuar histórias, eternizar
                     memórias e registrar emoções. Cada traço que faço é como uma
                     pincelada de personalidade que fica pra sempre.
@@ -55,7 +55,7 @@ function AboutMe() {
               </div>
 
               <div>
-                <span className="mt-3 block">
+                <span className="mt-3 block max-md:relative max-md:translate-x-[-70px]">
                   <Button
                     text="SOBRE MIM"
                     href="https://www.instagram.com/laiss.tattoo/"
